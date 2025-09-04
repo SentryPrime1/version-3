9413aa31880af8d284c11a822cf698ba61ed68e9
SentryPrime1 <gabeguglietti@gmail.com>
2025-09-03 20:02:41 -0600

    Update app.module.ts

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,7 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 import { ScanModule } from './scan/scan.module';
 
 @Module({
-  imports: [ScanModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    DatabaseModule,
+    ScanModule,
+  ],
 })
 export class AppModule {}

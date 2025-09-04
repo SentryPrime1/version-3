0302f653f3c9720e6dfdf1b6f2db5bb408c020bc
SentryPrime1 <gabeguglietti@gmail.com>
2025-09-04 14:52:02 -0600

    Update scan.module.ts

--- apps/backend/src/scan/scan.module.ts
+++ apps/backend/src/scan/scan.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ScanController } from './scan.controller';
 import { ScanService } from './scan.service';
+import { Scan } from '../entities/scan.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Scan])],
   controllers: [ScanController],
   providers: [ScanService],
 })
